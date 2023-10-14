e3e7cacae82bb302c8db9246dbff846717d0ada8
glows777 <glows778@gmail.com>
2023-10-15 01:04:25 +0800

    feat: initialize project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline">Click me</Button>
+    </div>
+  )
+}
---
+++ app/marketing/page.tsx
@@ -0,0 +1,15 @@
+import Heading from './_components/heading'
+
+const MarketingPage = () => {
+    return (
+        <div className="min-h-full flex flex-col">
+            <div className="flex flex-col items-center justify-center
+                mid:justify-start text-center gap-y-8 px-6 flex-1 pb-10"
+            >
+                <Heading />
+            </div>
+        </div>
+    )
+}
+ 
+export default MarketingPage
